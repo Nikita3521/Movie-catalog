21185578c41d91376987d921ba66e586647794d3
secretive20 <2fortnite2lol@gmail.com>
2026-01-03 02:21:08 +0200

    did details page

--- src/pages/ViewDetails.jsx
+++ src/pages/ViewDetails.jsx
@@ -1,5 +1,8 @@
+import styles from "../module/ViewDetails.module.css";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import star from "../img/MovieCatalog/v-icon.png";
+import logo from "../img/MovieCatalog/IMDBLogo.svg";
 
 export function ViewDetails() {
   const { id } = useParams();
@@ -18,13 +21,46 @@ export function ViewDetails() {
   }, [id]);
 
   return (
-    <div>
-      <h1 style={{ color: "white", fontSize: "24px" }}>{data.title}</h1>
-      <img
-        style={{ height: "300px", weight: "300px" }}
-        src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
-        alt=""
-      />
-    </div>
+    <>
+      <div className={styles.container}>
+        <img
+          style={{ height: "400px", width: "275px" }}
+          src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+          alt=""
+        />
+        <div className={styles.content}>
+          <ul style={{ display: "flex", gap: "7px" }}>
+            {data?.genres?.map((e) => (
+              <li key={e.id} className={styles.genres}>
+                {e.name}
+              </li>
+            ))}
+          </ul>
+          <h1 style={{ color: "white", fontSize: "34px", fontWeight: "600" }}>
+            {data.title}
+          </h1>
+          <div className={styles.rating}>
+            <img src={logo} alt="" />
+            {data.vote_average?.toFixed(1)}
+            <img src={star} alt="" />
+          </div>
+          <p className={styles.overview}>{data.overview}</p>
+          <div>
+            <ul className={styles.company}>
+              {data?.production_companies?.map((e) => (
+                <li>
+                  <img
+                    style={{ height: "40px", width: "40px" }}
+                    src={`https://image.tmdb.org/t/p/w500${e.logo_path}`}
+                    alt=""
+                  />
+                  {e.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
